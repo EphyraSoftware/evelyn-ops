eeb7e1232be4c8dce15b71ca7b0c54d49bcbadc1
ThetaSinner <greatdjonfire@hotmail.co.uk>
2019-10-19 13:42:13 +0100

    Run basic MongoDB on kubernetes

---
+++ platform/mongodb/setup.sh
@@ -0,0 +1,3 @@
+# Use this instead https://www.mongodb.com/kubernetes
+
+kubectl apply --namespace evelyn-platform -f mongo.yml
--- platform/rabbitmq/setup.sh
+++ platform/rabbitmq/setup.sh
@@ -1,1 +1,3 @@
+# Replace with https://content.pivotal.io/blog/introducing-rabbitmq-for-kubernetes when it's available.
+
 kubectl apply --namespace evelyn-platform -f rabbit.yml

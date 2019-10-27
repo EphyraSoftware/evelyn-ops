08a23761493d9d315ff11973e207fe7a029269c3
ThetaSinner <greatdjonfire@hotmail.co.uk>
2019-10-27 20:41:04 +0000

    Create deployment for greenmail

---
+++ platform/greenmail/setup.sh
@@ -0,0 +1,8 @@
+# To connect from Thunderbird use
+# address 'username@localhost'
+# password 'username@localhost'
+# server 'localhost'
+# (incoming) pop3 port '3110'
+# (outgoing) smtp port '3025'
+
+kubectl apply --namespace evelyn-platform -f greenmail.yml

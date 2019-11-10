a179b7165bcde8554e6385dafa2b8b0bb2b8c28b
ThetaSinner <greatdjonfire@hotmail.co.uk>
2019-11-10 22:41:39 +0000

    Secure keycloak with TLS

--- platform/keycloak/setup.sh
+++ platform/keycloak/setup.sh
@@ -1,3 +1,8 @@
+terraform init
+terraform apply -auto-approve
+
+kubectl -n evelyn-platform create secret generic keycloak-tls --from-file=./tls.key --from-file=./tls.crt
+
 kubectl apply --namespace evelyn-platform -f keycloak-postgres.yml
 
 kubectl apply -n evelyn-platform -f keycloak.yml

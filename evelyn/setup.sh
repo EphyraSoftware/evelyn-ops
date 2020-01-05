77b1c0b7b8611ac45b61483f8bad7c3e4740599c
ThetaSinner <greatdjonfire@hotmail.co.uk>
2020-01-05 23:03:54 +0000

    Add remaining deploy iac from evelyn-services

---
+++ evelyn/setup.sh
@@ -0,0 +1,2 @@
+kubectl apply -f main.yaml
+kubectl create secret docker-registry ghregcred --namespace evelyn-services --docker-server=https://docker.pkg.github.com/ephyrasoftware/evelyn-service --docker-username=ThetaSinner --docker-password=<your-pword> --docker-email=greatdjonfire@hotmail.co.uk

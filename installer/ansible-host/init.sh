71776bb02599c23fb1680a92c8b386b2f2e8706d
ThetaSinner <greatdjonfire@hotmail.co.uk>
2020-06-10 18:55:11 +0100

    configure edge-hosts.yaml for the edge environment

--- installer/ansible-host/init.sh
+++ installer/ansible-host/init.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/bash
 
+printf "nameserver 192.168.1.19" > /etc/resolv.conf
+
 printf "\192.168.1.2 nodea\n" >> /etc/hosts
 printf "\n192.168.1.15 nodeb\n" >> /etc/hosts
-
-ssh -i /ssh/id_rsa thetasinner@nodea exit
-ssh -i /ssh/id_rsa thetasinner@nodeb exit

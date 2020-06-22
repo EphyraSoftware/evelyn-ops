488090f415a3c1d6d4fbbf75840a8bfae606617a
ThetaSinner <greatdjonfire@hotmail.co.uk>
2020-06-22 01:30:05 +0100

    Allow Configure-ShellSession.ps1 to be run from any directory

--- packages/vault/environments/edge/Configure-ShellSession.ps1
+++ packages/vault/environments/edge/Configure-ShellSession.ps1
@@ -1,3 +1,4 @@
-$token = .\Get-RootToken.ps1
+$scriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+$token = $(& "$scriptDir\Get-RootToken.ps1")
 $env:VAULT_TOKEN="$token"
 $env:VAULT_ADDR='http://localhost:8200'

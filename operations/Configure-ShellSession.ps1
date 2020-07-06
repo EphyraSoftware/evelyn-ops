485e1553c28e77f9dd8884ec514da904e9f86004
ThetaSinner <greatdjonfire@hotmail.co.uk>
2020-07-06 20:29:39 +0100

    Add registry credentials to the shell configuration

--- operations/Configure-ShellSession.ps1
+++ operations/Configure-ShellSession.ps1
@@ -1,3 +1,4 @@
 $scriptDir = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
 & "$scriptDir\..\packages\vault\environments\edge\Configure-ShellSession.ps1"
 & "$scriptDir\..\packages\evelyn-ci\environments\edge\Configure-BuildKitSession.ps1"
+& "$scriptDir\..\packages\evelyn-ci\environments\edge\Export-RegistryCredentials.ps1"
